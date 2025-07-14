80d0b6f9aad28e2d9b7542815f8823711a41bb83
sorrynogmailauthentication <mike.volchek@gmail.com>
2025-07-14 16:14:04 +0300

    corrected-mistake

--- index.ts
+++ index.ts
@@ -15,7 +15,7 @@ function checkArgs(min: number, max: number, length: number) {
 const {argv} = process;
 
 const length: number = +argv[2] >= 1 ? +argv[2] : 7;
-const min: number = +argv[4] && typeof +argv[3] === 'number' ? +argv[3] : 1;
+const min: number = +argv[3] && typeof +argv[3] === 'number' ? +argv[3] : 1;
 const max: number = +argv[4] && typeof +argv[4] === 'number' ? +argv[4] : 49;
 const [checkedMin, checkedMax] = checkArgs(min, max, length)
 const a: number[] = _.sampleSize(_.range(checkedMin, checkedMax + 1), length);
