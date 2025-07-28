80ff4902d6538d723b616274157355babddbc8c4
sorrynogmailauthentication <mike.volchek@gmail.com>
2025-07-28 13:40:51 +0300

    read-config-once

--- Logger.ts
+++ Logger.ts
@@ -2,6 +2,12 @@ import EventEmitter from "events";
 import config from 'config';
 
 export type LogLevel = "severe"|"warn"|"info"|"debug"|"trace"
+
+const LOG_LEVELS = ["severe", "warn", "info", "debug", "trace"];
+const LOG_LEVEL_CONFIG = config.get<LogLevel>("log_level");
+const LOG_LEVEL_INDEX = LOG_LEVELS.indexOf(LOG_LEVEL_CONFIG);
+const DEFAULT_LOG_LEVEL = "info";
+
 class Logger extends EventEmitter {
      addHandlerLevel(level: LogLevel, handler: (message: string)=>void): void {
         this.on(level, handler);
@@ -9,13 +15,11 @@ class Logger extends EventEmitter {
      addHandlerMessage(handler:(obj: {level: LogLevel, message: string})=>void):void {
         this.on("message", handler);
      }
-     log(level: LogLevel = "info", message: string): void {
-        const logLevel = config.get<LogLevel>("log_level");
-        const logLevelArray = ['severe', 'warn', 'info', 'debug', 'trace']
-        const logLevelIndex = logLevelArray.indexOf(logLevel)
-        const levelIndex = logLevelArray.indexOf(level)
-        if (levelIndex <= logLevelIndex) {
+     log(level: LogLevel, message: string): void {
+        level = level ?? DEFAULT_LOG_LEVEL;
+        if (LOG_LEVELS.indexOf(level) <= LOG_LEVEL_INDEX) {
          this.emit(level, message);
+         console.log(level.toUpperCase());
          this.emit("message", {level, message});
         }
       }
--- index.ts
+++ index.ts
@@ -3,32 +3,38 @@ import fs from "node:fs"
 
 const logger = new Logger();
 
-console.log("test 1 - log level is warn, message is debug, expected output: nothing")
+console.log("test 1 - log level is info, message is debug, expected output: nothing")
 logger.addHandlerMessage((obj) => console.log(obj.message));
 logger.addHandlerLevel("debug", (message) => fs.writeFileSync("logs.txt", '\n' + message, {flag:"a"}))
 logger.log("debug", "kukureku");
 logger.removeAllListeners();
 
-console.log("test 2 - log level is warn, message is info, expected output: nothing")
+console.log("test 2 - log level is info, message is info, expected output: info message")
 logger.addHandlerMessage((obj) => console.log(obj.message));
 logger.addHandlerLevel("info", (message) => fs.writeFileSync("logs.txt", '\n' + message, {flag:"a"}))
 logger.log("info", "kukureku");
 logger.removeAllListeners();
 
-console.log("test 3 - log level is warn, message is warn, expected output: warn message")
+console.log("test 3 - log level is info, message is warn, expected output: warn message")
 logger.addHandlerMessage((obj) => console.log(obj.message));
 logger.addHandlerLevel("warn", (message) => fs.writeFileSync("logs.txt", '\n' + message, {flag:"a"}))
 logger.log("warn", "kukureku");
 logger.removeAllListeners();
 
-console.log("test 4 - log level is warn, message is trace, expected output: nothing")
+console.log("test 4 - log level is info, message is trace, expected output: nothing")
 logger.addHandlerMessage((obj) => console.log(obj.message));
 logger.addHandlerLevel("trace", (message) => fs.writeFileSync("logs.txt", '\n' + message, {flag:"a"}))
 logger.log("trace", "kukureku");
 logger.removeAllListeners();
 
-console.log("test 5 - log level is warn, message is severe, expected output: severe message")
+console.log("test 5 - log level is info, message is severe, expected output: severe message")
 logger.addHandlerMessage((obj) => console.log(obj.message));
 logger.addHandlerLevel("severe", (message) => fs.writeFileSync("logs.txt", '\n' + message, {flag:"a"}))
 logger.log("severe", "kukureku");
+logger.removeAllListeners();
+
+console.log("test 6 - log level is info, message is undefined, expected output: info message")
+logger.addHandlerMessage((obj) => console.log(obj.message));
+logger.addHandlerLevel(undefined, (message) => fs.writeFileSync("logs.txt", '\n' + message, {flag:"a"}))
+logger.log(undefined, "kukureku");
 logger.removeAllListeners();
