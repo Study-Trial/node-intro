9dc16de84e6e1f0ac6f764bdf342b33af5900053
sorrynogmailauthentication <mike.volchek@gmail.com>
2025-08-06 16:10:07 +0300

    initial commit

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,45 +1,44 @@
-import http from 'node:http';
-import { calculate } from '../service/compute.ts';
 
-function validateArgs(args: any) {
-   if (args.operation === undefined || args.op1 === undefined || args.op2 === undefined) {
-      throw new Error(`Undefined argument found: argument 1 is ${args.operation}, argument 2 is ${args.op1}, argument 3 is ${args.op2}`);
-   }
-}
+import { validation } from '../middleware/validation.ts';
+import CalculationData from '../model/CalculationData.ts';
+import calculator, { WrongOperationError } from '../service/calculator.ts'
+import express ,{Response, Request} from 'express'
 
-function validateOperation(operation: string) {
-   if (typeof operation !== "string") {
-      throw new Error(`Operation is not a string: ${typeof operation}`);
-   }
-}
-
-function validateOperands(op1: number, op2: number) {
-   if (typeof op1 !== "number" || typeof op2 !== "number") {
-      throw new Error(`Operands are not both numbers: operand 1 is ${typeof op1}, operand 2 is ${typeof op2}`);
-   }
-}
-
-const server = http.createServer();
 const port = 3500;
-server.listen(port, () => console.log('listening on port ' + port));
-server.on("request", async (req, res) => {
-   let data = "";
-   for await (let chunk of req) {
-      data += chunk;
-   }
-   try {
-      const args = JSON.parse(data);
-      validateArgs(args);
-      const { operation, op1, op2 } = args;
-      validateOperation(operation);
-      validateOperands(op1, op2);
-      const result = calculate(operation, op1, op2);
-      res.statusCode = 200;
-      res.end(result.toString());
-   } catch (error) {
-      res.statusCode = 400;
-      res.end(error.message);
-   }
-})
 
+const app = express();
+app.listen(port);
+app.use(express.json());
+app.use(validation)
+app.post("/api/calculator", (req: Request & {error: Error}, res: Response) => {
+ try {
+   if(!req.body) {
+      throw req.error
+   }
+    const result = calculator.calculate(req.body as CalculationData)
+    sendResponse(res, 200, result);
+ } catch (error) {
+   const status = error instanceof WrongOperationError ? 404 : 400;
+   sendResponse(res, status, error.message)
+
+ }
+})
+app.get("/api/calculator/:operation/:op1/:op2", validation, (req: Request & {error: Error}, res: Response) => {
+   try {
+      if(!req.body) {
+      throw req.error
+   } 
+    const result = calculator.calculate(req.params as any)
+    sendResponse(res, 200, result);
+ } catch (error) {
+   const status = error instanceof WrongOperationError ? 404 : 400;
+   sendResponse(res, status, error.message)
+
+ }
+
+})
+function sendResponse(res: Response, status: number, result: number | string) {
+      res.statusCode = status;
+      res.send(result)
+}
 
